361ca2c6debcbed3917332598f31a75ab11bc64f
diogenesjcs <diogenesjcs@gmail.com>
2017-05-16 17:05:09 -0300

    Corrigido bug na requisição de obras e adicionada configuração de linter.

--- controllers/complaints.js
+++ controllers/complaints.js
@@ -1,4 +1,3 @@
-'use strict';
 const passport = require('passport');
 const Complaint = require('../models/Complaint');
 const Location = require('../models/Location');
@@ -6,75 +5,80 @@ const ConstructionSite = require('../models/ConstructionSite');
 const User = require('../models/User');
 
 exports.postAddComplaint = (req, res, next) => {
-    var coords = [];
-    coords[0] = req.body.lng;
-    coords[1] = req.body.lat;
-    var query = {
-            lat: coords[1],
-            lng: coords[2]
-        },
-        update = {
-            expire: new Date()
-        },
-        options = {
-            upsert: true,
-            new: true,
-            setDefaultsOnInsert: true
-        };
-    Location.findOneAndUpdate(query, update, options, function(error, location) {
-        if (error) return next(err);
-        User.findOne({
-            email: req.body.email
-        }, (err, user) => {
-            if (err) {
-                return next(err);
+  const coords = [];
+  coords[0] = req.body.lng;
+  coords[1] = req.body.lat;
+  const query = {
+    lat: coords[1],
+    lng: coords[2]
+  };
+  const update = {
+    expire: new Date()
+  };
+  const options = {
+    upsert: true,
+    new: true,
+    setDefaultsOnInsert: true
+  };
+  Location.findOneAndUpdate(query, update, options, (error, location) => {
+    if (error) return next(error);
+    User.findOne(
+      {
+        email: req.body.email
+      },
+      (err, user) => {
+        if (err) {
+          return next(err);
+        }
+        ConstructionSite.findOne(
+          {
+            email: req.body.constructionSiteId
+          },
+          (err2, constructionSite) => {
+            if (err2) {
+              return next(err);
             }
-            ConstructionSite.findOne({
-                email: req.body.constructionSiteId
-            }, (err2, constructionSite) => {
-                if (err2) {
-                    return next(err);
-                }
-                constructionSite.complaints += 1;
-                constructionSite.save();
-                const complaint = new Complaint({
-                    location: location._id,
-                    createdBy: user._id,
-                    impact: 0,
-                    images: [],
-                    constructionSite: constructionSite._id
-                });
-                complaint.save();
+            constructionSite.complaints += 1;
+            constructionSite.save();
+            const complaint = new Complaint({
+              location: location._id,
+              createdBy: user._id,
+              impact: 0,
+              images: [],
+              constructionSite: constructionSite._id
             });
-        });
-    });
-
+            complaint.save();
+          }
+        );
+      }
+    );
+  });
 };
 
 exports.getConstructionSites = (req, res) => {
-    ConstructionSite.find({}, function(err, constructionSites) {
-        if (err) {
-            return next(err);
-        }
-        var constructionSitesMap = {};
+  ConstructionSite.find({}, (err, constructionSites) => {
+    if (err) {
+      return err;
+    }
+    const constructionSitesMap = [];
 
-        constructionSites.forEach(function(constructionSite) {
-            constructionSitesMap.push(constructionSite);
-        });
-        res.send(constructionSitesMap);
+    constructionSites.forEach((constructionSite) => {
+      constructionSitesMap.push(constructionSite);
     });
+    res.send(constructionSitesMap);
+  });
 };
 
 exports.getComplaints = (req, res) => {
-    Complaint.find({}, function(err, complaints) {
-        if (err) {
-            return next(err);
-        }
-        var complaintsMap = {};
+  Complaint.find({}, (err, complaints) => {
+    if (err) {
+      return err;
+    }
+    const complaintsMap = [];
 
-        complaints.forEach(function(complaint) {
-            complaintsMap.push(complaint);
-        });
-        res.send(complaintsMap);
+    complaints.forEach((complaint) => {
+      complaintsMap.push(complaint);
     });
+    res.send(complaintsMap);
+  });
 };
