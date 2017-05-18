9d5bba22e512bbe723d440a83b6f2c2383926885
diogenesjcs <diogenesjcs@gmail.com>
2017-05-18 12:47:24 -0300

    Corrigido formato da data das denúncias, dados do usuário retornam com a denúncia agora.

--- controllers/complaints.js
+++ controllers/complaints.js
@@ -2,46 +2,46 @@ const passport = require('passport');
 const Complaint = require('../models/Complaint');
 const ConstructionSite = require('../models/ConstructionSite');
 const User = require('../models/User');
+const Promise = require('bluebird');
+const _ = require('lodash');
 
 exports.postAddComplaint = (req, res, next) => {
   const coords = [];
   coords[0] = req.body.lng;
   coords[1] = req.body.lat;
-  User.findOne(
-    {
-      email: req.body.email
-    },
-    (err, user) => {
-      if (err) {
-        return next(err);
-      }
-      ConstructionSite.findOne(
-        {
-          _id: req.body.constructionSiteId
-        },
-        (err2, constructionSite) => {
-          if (err2) {
+  User.findOne({
+    email: req.body.email
+  },
+        (err, user) => {
+          if (err) {
             return next(err);
           }
-          if (constructionSite.complaints !== null) constructionSite.complaints++;
-          else constructionSite.complaints = 1;
+          ConstructionSite.findOne({
+            _id: req.body.constructionSiteId
+          },
+                (err2, constructionSite) => {
+                  if (err2) {
+                    return next(err);
+                  }
+                  if (constructionSite.complaints !== null) constructionSite.complaints++;
+                  else constructionSite.complaints = 1;
 
-          constructionSite.save();
-          const complaint = new Complaint({
-            lat: coords[1],
-            lng: coords[0],
-            createdBy: user._id,
-            impact: req.body.impact,
-            images: [],
-            description: req.body.description,
-            constructionSite: constructionSite._id
-          });
-          complaint.save();
-          res.send(complaint);
+                  constructionSite.save();
+                  const complaint = new Complaint({
+                    lat: coords[1],
+                    lng: coords[0],
+                    createdBy: user._id,
+                    impact: req.body.impact,
+                    images: [],
+                    description: req.body.description,
+                    constructionSite: constructionSite._id
+                  });
+                  complaint.save();
+                  res.send(complaint);
+                }
+            );
         }
-      );
-    }
-  );
+    );
 };
 
 exports.getConstructionSites = (req, res) => {
@@ -59,38 +59,52 @@ exports.getConstructionSites = (req, res) => {
 
 exports.getComplaints = (req, res) => {
   if (req.query.email) {
-    User.findOne(
-      {
-        email: req.body.email
-      },
-      (err, user) => {
-        if (err) {
-          return next(err);
-        }
-        Complaint.find({ createdBy: user._id }, (err, complaints) => {
-          if (err) {
-            return err;
-          }
-          const complaintsMap = [];
+    User.findOne({
+      email: req.body.email
+    },
+            (err, user) => {
+              if (err) {
+                return err;
+              }
+              Complaint.find({ createdBy: user._id }, (err, complaints) => {
+                if (err) {
+                  return err;
+                }
+                const complaintsMap = [];
 
-          complaints.forEach((complaint) => {
-            complaintsMap.push(complaint);
-          });
-          res.send(complaintsMap);
-        });
-      }
-    );
+                complaints.forEach((complaint) => {
+                  complaintsMap.push(complaint);
+                });
+                res.send(complaintsMap);
+              });
+            }
+        );
   } else {
     Complaint.find({}, (err, complaints) => {
       if (err) {
         return err;
       }
       const complaintsMap = [];
+      const users = [];
 
       complaints.forEach((complaint) => {
         complaintsMap.push(complaint);
+        users.push(User.findOne({ _id: complaint.createdBy }).exec());
       });
-      res.send(complaintsMap);
+      Promise.all(users).then((resultUsers) => {
+        const complaintsResult = _.map(complaintsMap, (comp) => {
+          const model = {
+            email: null,
+            profile: null,
+            _id: null
+          };
+          const user = _.pick(_.find(resultUsers, u => (u._id.toString() === comp.createdBy)), _.keys(model));
+          let c = JSON.parse(JSON.stringify(comp));
+          c.createdBy = user;
+          return c;
+        });
+        res.send(complaintsResult);
+      });
     });
   }
 };
