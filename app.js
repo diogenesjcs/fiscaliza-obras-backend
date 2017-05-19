fbd7994dbcaadd452aad8315085a930e6d9e6e86
diogenesjcs <diogenesjcs@gmail.com>
2017-05-19 01:18:48 -0300

    Removida manipulação de sessão.

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
 });
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     // After successful login, redirect back to the intended page
   if (!req.user &&
         req.path !== '/login' &&
@@ -131,7 +131,7 @@ app.use((req, res, next) => {
     req.session.returnTo = req.path;
   }
   next();
-});
+});*/
 app.use(
     express.static(path.join(__dirname, 'public'), {
       maxAge: 31557600000
