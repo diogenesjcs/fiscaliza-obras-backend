86057bb16009f4ba4474fb25c891f65f044cd170
diogenesjcs <diogenesjcs@gmail.com>
2017-05-19 10:36:38 -0300

    Erro de digitação

--- app.js
+++ app.js
@@ -77,12 +77,6 @@ app.use(
     })
 );
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(
-    bodyParser.urlencoded({
-      extended: true
-    })
-);
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(expressValidator());
